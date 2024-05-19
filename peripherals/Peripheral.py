baf7cb4e1fce4a5a74e6fa6d526b8b4f65e9e566
Zuddas-s <email@youremail.com>
2024-05-19 18:58:46 +0100

    creating abstract classes for a neater implementation of sensors and actuators

---
+++ peripherals/Peripheral.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Peripheral(ABC):
+
+    @abstractmethod
+    def initialise_attributes_from_df(self):
+        pass
+
+    @abstractmethod
+    def get_latest_state(self):
+        pass
+
+    @abstractmethod
+    def get_timeseries(self):
+        pass
+
+    @abstractmethod
+    def get_state_at_time(self, time):
+        pass
+
+    @abstractmethod
+    def plot(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
